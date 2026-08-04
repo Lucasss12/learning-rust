f024e34ce6b54a6ff5b5413f6094c161a1b16457
Lucasss12 <lucas.mene.pro@gmail.com>
2026-08-04 16:52:38 +0200

    The Book: Chap8

---
+++ chap_8/vecteurs/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let v = vec![1, 2, 3, 4, 5];
+
+    let troisieme: &i32 = &v[2];
+    println!("Le troisième élément est {}", troisieme);
+
+    match v.get(2) {
+        Some(troisieme) => println!("Le troisième élément est {}", troisieme),
+        None => println!("Il n'y a pas de troisième élément."),
+    }
+}
